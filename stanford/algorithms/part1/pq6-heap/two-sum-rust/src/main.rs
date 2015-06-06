f2ad36a95f92cd5a21013298b920c77ad48fa7f3
Nikica Jokic <neektza@gmail.com>
2015-06-06 23:41:42 +0200

    2SUM satisfies simple test cases but halts on actual input.

--- stanford/algorithms/part1/pq6-heap/two-sum-rust/src/main.rs
+++ stanford/algorithms/part1/pq6-heap/two-sum-rust/src/main.rs
@@ -1,12 +1,17 @@
-use std::error::Error;
+use std::env;
 use std::fs::File;
+use std::path::Path;
 use std::io::BufReader;
 use std::io::prelude::*;
-use std::path::Path;
+use std::collections::HashMap;
+use std::error::Error;
 
 fn main() {
     // Create a path to the desired file
-    let path = Path::new("input/p1_myinput1.txt");
+
+    let path_str = env::args().nth(1).unwrap();
+
+    let path = Path::new(&path_str);
     let display = path.display();
 
     // Open the path in read-only mode, returns `io::Result<File>`
@@ -21,6 +26,25 @@ fn main() {
     let nums: Vec<i64> = reader.lines().map(|l| {
         l.ok().and_then(|s| s.parse().ok()).unwrap_or(0)
     }).collect();
+        
+    println!("input consumed!");
 
-    println!("{:?}", nums);
+    let mut cnt = 0;
+
+    for t in -10000..10001 {
+        print!("t:{: <10}\r", t);
+        let mut sums: HashMap<i64, i64> = HashMap::new();
+
+        for x in nums.iter() { 
+            if sums.contains_key(x) {
+                // println!("{} = {} + {}", t, t-x, x);
+                cnt += 1;
+                break;
+            } else {
+                sums.insert(t-x, 1);
+            }
+        }
+    }
+
+    println!("\nnumber of t-sums {}", cnt);
 }
